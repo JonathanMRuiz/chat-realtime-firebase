72ebc5541be8c7b1cf281d04bbe234701f36c122
JonathanMRuiz <jonimrz@yahoo.com>
2023-02-09 15:50:39 -0300

    navbar app

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { auth } from "../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { GoogleAuthProvider, signInWithRedirect, signOut } from "firebase/auth";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
-};
+  const [user] = useAuthState(auth);
 
+  const googleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithRedirect(auth, provider);
+  };
+
+  const logout = () => {
+    return signOut(auth);
+  };
+  return (
+    <nav className="nav-bar">
+      <h1>React Chat</h1>
+      {user ? (
+        <button onClick={logout} className="sign-out" type="button">
+          Sign Out
+        </button>
+      ) : (
+        <button className="sign-in" onClick={googleSignIn}>
+          Sign in with Google
+        </button>
+      )}
+    </nav>
+  );
+};
 export default NavBar;
